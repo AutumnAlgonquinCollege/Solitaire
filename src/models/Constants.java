1168b173eb31b02b9a7fb9bb7654bf0238f2ef19
AutumnAlgonquinCollege <more0328@algonquinlive.com>
2024-02-06 10:25:21 -0500

    Fixed arraylength bug

--- src/models/Constants.java
+++ src/models/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	//==================================
 	//        List of Constants
 	//==================================
-	public static final String[] suitArray = {"SPADES, CLUBS, DIAMONDS, HEARTS"};
+	public static final String[] suitArray = {"SPADES", "CLUBS", "DIAMONDS", "HEARTS"};
 	public static final int[] rankArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 	public static final String spades = "SPADES";
 	public static final String clubs = "CLUBS";
